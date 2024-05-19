39f8f71737fcf3e66d619e2dffc984dd345f18c9
Aycan <aycan.biilge@gmail.com>
2024-05-19 17:15:39 -0400

    added to salesContract class

--- src/main/java/com/ps/Contract.java
+++ src/main/java/com/ps/Contract.java
@@ -1,4 +1,68 @@
 package com.ps;
 
-public abstract class Contract {
+public class Contract {
+    protected String date;
+    protected String customer_name;
+    protected String customer_email;
+    protected String vehicle_sold;
+    protected double total_price;
+    protected double monthly_payment;
+
+    public Contract(String date, String customer_name, String customer_email, String vehicle_sold, double total_price, double monthly_payment) {
+        this.date = date;
+        this.customer_name = customer_name;
+        this.customer_email = customer_email;
+        this.vehicle_sold = vehicle_sold;
+        this.total_price = total_price;
+        this.monthly_payment = monthly_payment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_email() {
+        return customer_email;
+    }
+
+    public void setCustomer_email(String customer_email) {
+        this.customer_email = customer_email;
+    }
+
+    public String getVehicle_sold() {
+        return vehicle_sold;
+    }
+
+    public void setVehicle_sold(String vehicle_sold) {
+        this.vehicle_sold = vehicle_sold;
+    }
+
+    public void getTotal_price() {
+
+    }
+
+    public void setTotal_price(double total_price) {
+        this.total_price = total_price;
+    }
+
+    public void getMonthly_payment() {
+
+    }
+
+    public void setMonthly_payment(double monthly_payment) {
+        this.monthly_payment = monthly_payment;
+    }
 }
+
--- src/main/java/com/ps/SalesContract.java
+++ src/main/java/com/ps/SalesContract.java
@@ -1,12 +1,52 @@
 package com.ps;
 
 public class SalesContract extends Contract{
+    private double sales_tax_amount;
+    private double recording_fee;
+    private double processing_fee;
+    private boolean finance;
+    private double monthly_payment;
 
-    protected String date;
-    protected String customer_name;
-    protected String customer_email;
-    protected String vehicle_sold;
-    protected double total_price;
-    protected double monthly_payment;
+    public SalesContract(String date, String customer_name, String customer_email, String vehicle_sold, double total_price, double monthly_payment, double sales_tax_amount, double recording_fee, double prcoessing_fee, boolean finance, double monthly_payment1) {
+        super(date, customer_name, customer_email, vehicle_sold, total_price, monthly_payment);
+        this.sales_tax_amount = sales_tax_amount;
+        this.recording_fee = recording_fee;
+        this.processing_fee = processing_fee;
+        this.finance = finance;
+        this.monthly_payment = monthly_payment1;
+    }
+
+    public double getSales_tax_amount() {
+        return sales_tax_amount;
+    }
+
+    public void setSales_tax_amount(double sales_tax_amount) {
+        this.sales_tax_amount = sales_tax_amount;
+    }
+
+    public double getRecording_fee() {
+        return recording_fee;
+    }
+
+    public void setRecording_fee(double recording_fee) {
+        this.recording_fee = recording_fee;
+    }
+
+    public double getProcessing_fee() {
+        return processing_fee;
+    }
+
+    public void setProcessing_fee(double processing_fee) {
+        this.processing_fee = processing_fee;
+    }
+
+    public boolean isFinance() {
+        return finance;
+    }
+
+    public void setFinance(boolean finance) {
+        this.finance = finance;
+    }
+
 
 }
